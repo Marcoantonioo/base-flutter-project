1fcf57659da8926c0697ab42492cd0fd471996bc
Marco Antonio Lentz <marco.lentz2@gmail.com>
2022-02-15 19:31:04 -0300

    feat: Criado componente para lidar com o ciclo de vida do app

--- lib/app_injector.dart
+++ lib/app_injector.dart
@@ -1,5 +1,6 @@
 import 'package:base_flutter_project/architecture/architecture_di.dart';
 import 'package:base_flutter_project/architecture/injection/injector.dart';
+import 'package:base_flutter_project/components/components_di.dart';
 import 'package:base_flutter_project/core/core_di.dart';
 import 'package:base_flutter_project/presentation/presentation_injector.dart';
 
@@ -7,6 +8,7 @@ class AppInjector extends Injector {
   @override
   void dependencies() {
     ArchitectureInjector().dependencies();
+    ComponentsInjector().dependencies();
     CoreInjector().dependencies();
     PresentationInjector().dependencies();
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,16 @@
-import 'package:base_flutter_project/app_injector.dart';
-import 'package:base_flutter_project/presentation/screens/login/login_screen.dart';
-import 'package:base_flutter_project/routes/route_name.dart';
-import 'package:base_flutter_project/routes/routes.dart';
+import 'package:base_flutter_project/components/app_state/app_state_component.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Base App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialBinding: AppInjector(),
-      debugShowCheckedModeBanner: false,
-      getPages: routes,
-      initialRoute: RouteName.login,
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
+
+class _MyAppState extends AppStateComponent {}
