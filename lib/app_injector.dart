0efac8678adc544f408b99645ecc21342b2c506e
Marco Antonio Lentz <marco.lentz2@gmail.com>
2022-02-21 21:52:23 -0300

    fix: Ajustes realizados

--- lib/app_injector.dart
+++ lib/app_injector.dart
@@ -2,7 +2,6 @@ import 'package:base_flutter_project/architecture/architecture_di.dart';
 import 'package:base_flutter_project/architecture/injection/injector.dart';
 import 'package:base_flutter_project/components/components_di.dart';
 import 'package:base_flutter_project/core/core_di.dart';
-import 'package:base_flutter_project/presentation/presentation_injector.dart';
 
 class AppInjector extends Injector {
   @override
@@ -10,6 +9,5 @@ class AppInjector extends Injector {
     ArchitectureInjector().dependencies();
     ComponentsInjector().dependencies();
     CoreInjector().dependencies();
-    PresentationInjector().dependencies();
   }
 }
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,3 +1,4 @@
+import 'package:base_flutter_project/presentation/screens/login/login_injector.dart';
 import 'package:base_flutter_project/presentation/screens/login/login_screen.dart';
 import 'package:base_flutter_project/routes/route_name.dart';
 import 'package:get/get.dart';
@@ -6,5 +7,6 @@ final List<GetPage> routes = [
   GetPage(
     name: RouteName.login,
     page: () => const LoginScreen(),
+    binding: LoginInjector()
   ),
 ];
